0141de27bd590fbc3161c2e87555adf48909a186
Dylan Cashman <depcashman@gmail.com>
2023-03-23 17:19:50 -0400

    adding square calculator

---
+++ specmetric/position_calculators/squarifier.py
@@ -0,0 +1,30 @@
+import squarify
+import pandas as pd
+
+def squarify_within_bar(ids, values, width, height, pad=True):
+  """Uses the squarify package (https://github.com/laserson/squarify)
+  to get coordinates for squares that fit within a bar of 
+  width=width and height=height.  Returns df with locations and
+  distances relative to the top left of the bar
+  """
+  # these values define the coordinate system for the returned rectangles
+  # the values will range from x to x + width and y to y + height
+  x = 0.
+  y = 0.
+
+  # values must be sorted descending (and positive, obviously)
+  # values.sort(reverse=True)
+  values.sort()
+  values = values[::-1]
+
+  # the sum of the values must equal the total area to be laid out
+  # i.e., sum(values) == width * height
+  values = squarify.normalize_sizes(values, width, height)
+
+  if pad:
+    # padded rectangles will probably visualize better for certain cases
+    rects = squarify.padded_squarify(values, x, y, width, height)
+  else:
+    rects = squarify.squarify(values, x, y, width, height)
+
+  return pd.DataFrame(data=rects, index=ids)
--- specmetric/visualization_container.py
+++ specmetric/visualization_container.py
@@ -11,29 +11,131 @@ class VisualizationContainer:
   """  
 
   def __init__(self, initial_node, compositions=_compositions, grammatical_expressions=_grammatical_expressions):
+    self.initial_node = initial_node
     self.computation_nodes = [initial_node]
-    self.mark_type = 'point'
-    self.mark_encodings = []
-    self.preferences = {}
+    self.valid_chart = None
+    self.encodings = {}
     self.compositions = compositions
-    self.grammatical_expressions=grammatical_expressions
+    self.grammatical_expressions = grammatical_expressions
     self.parse_initial_node_preferences()
 
   def get_function_preferences(self):
     return [self.grammatical_expressions[n.function_type] for n in self.computation_nodes]
 
   def parse_initial_node_preferences(self):
-    self.preferences = self.get_function_preferences()[0]
-    if (len(self.preferences['mark_priority']) > 0):
-      preference = self.preferences['mark_priority'][0]
-      self.mark_type = preference['output_data_mark']
-      # self.mark_encodings.append({
-      #   'channel': '',
-      #   'field': ''
-      #   })
+    preferences = self.get_function_preferences()[0]
 
-  def add_computation_nodes(node):
-    self.computation_nodes.append(node)
+    # Get any other encodings from the node
+    encoding_config = preferences['output_encoding_config']
+    if encoding_config:
+      self.update_encoding(self.initial_node.output_data, encoding_config)
+
+  def parse_chart(self, chart_type, input_data, output_data):
+    """
+    Adds the encodings for the selected chart type.  
+    """
+    if chart_type == 'spacefilling':
+      # input_data should be a vector name
+      # We should take input data's encoding.  We won't have that just yet.
+      # For now, I will just enforce that the child encoding gets squares
+      self.update_encodings(
+        input_data[0],
+        {
+          'mark': 'square',
+          'channels': ['x', 'y', 'x2', 'y2']
+        }
+      )
+      # Then, output data should be a scalar, encoded with an area
+      self.update_encodings(
+        output_data[0],
+        {
+          'mark': 'area',
+          'channels': ['x', 'y', 'x2', 'y2']
+        }
+      )
+
+      # The actual calculations of the values are calculated by the renderer
+    elif chart_type == 'single_stacked_bar':
+      # input_data are scalars, output_data is a single scalar
+      # inputs are areas, output is area
+      self.update_encodings(
+        input_data[0],
+        {
+          'mark': 'area',
+          'channels': ['x', 'y', 'x2', 'y2']
+        }
+      )
+
+      self.update_encodings(
+        input_data[1],
+        {
+          'mark': 'area',
+          'channels': ['x', 'y', 'x2', 'y2']
+        }
+      )
+
+      self.update_encodings(
+        output_data[0],
+        {
+          'mark': 'area',
+          'channels': ['x', 'y', 'x2', 'y2']
+        }
+      )
+    elif chart_type == 'scatter_y_equals_x':
+      # Inputs are points on scatterplot
+      # outputs are lines
+      self.update_encodings(
+        input_data[0],
+        {
+          'mark': 'point',
+          'channels': ['x', 'y']
+        }
+      )
+      self.update_encodings(
+        input_data[1],
+        {
+          'mark': 'point',
+          'channels': ['x', 'y']
+        }
+      )
+      self.update_encodings(
+        output_data[0],
+        {
+          'mark': 'line',
+          'channels': ['x', 'y', 'x2', 'y2']
+        }
+      )
+    elif chart_type == 'bar_chart_diff':
+      # inputs are scalars, output is scalar
+      # each is a bar with x and y value
+      self.update_encodings(
+        input_data[0],
+        {
+          'mark': 'line',
+          'channels': ['x', 'y']
+        }
+      )
+      self.update_encodings(
+        input_data[1],
+        {
+          'mark': 'line',
+          'channels': ['x', 'y']
+        }
+      )
+      self.update_encodings(
+        output_data[0],
+        {
+          'mark': 'line',
+          'channels': ['x', 'y']
+        }
+      )
+
+  def update_encoding(self, attribute_name, encoding_config, overwrite=True):
+    self.encodings[attribute_name] = self.encodings[attribute_name] or {}
+    self.encodings[attribute_name].merge(encoding_config)
+
+  # def add_computation_nodes(node):
+  #   self.computation_nodes.append(node)
 
   def parent_mergeable(self, parent_node):
     rule_match = VisualizationRule(
@@ -52,7 +154,12 @@ class VisualizationContainer:
       grammatical_expressions=grammatical_expressions
     )
 
+    if self.valid_chart and self.chart_reqs_met():
+      self.initial_parse_chart(self.valid_chart, self.initial_node.input_data, self.initial_node.output_data)
+
     self.preferences = rule_match.resolve_preferences()
     self.add_computation_node(parent_node)
     return self
   
+  def chart_reqs_met(self):
+    
